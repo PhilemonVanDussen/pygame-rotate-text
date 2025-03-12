ce06ba879c4b1d0a28c0321d9fb7d9cb9301175c
PhilemonVanDussen <vandussenph709@northwested.org>
2025-03-12 13:52:22 -0400

    yes

--- main.py
+++ main.py
@@ -20,6 +20,23 @@ def handle_events():
                 return False
     return True
 
+def draw_text(screen, text, font_size, color, x, y, font_name, italic=False, bold=False, rotation=0):
+    if font_name:
+        font = pygame.font.Font(font_name, font_size)
+    else:
+        font = pygame.font.Font(None, font_size)
+    
+    font.set_bold(bold)
+    font.set_italic(italic)
+    
+    text_surface = font.render(text, True, color)
+
+    if rotation != 0:
+        text_surface = pygame.transform.rotate(text_surface, rotation)
+
+    text_rect = text_surface.get_rect(center=(x,y))
+    screen.blit(text_surface, text_rect.topleft)
+        
 def main():
     screen = init_game()
     clock = pygame.time.Clock() # Initalize the clock here
@@ -27,6 +44,11 @@ def main():
     while running:
         running = handle_events()
         screen.fill(config.GREEN) # Use color from config
+
+        draw_text(screen, 'Hello', 50, config.CYAN, 300, 200, 'c:\Font\PlaywriteHU-VariableFont_wght.ttf', italic=True)
+        draw_text(screen, 'YO!', 70, config.BLACK, 300, 400, None, bold=True, rotation=40)
+        draw_text(screen, 'Whats GOOD', 90, config.PURPLE, 600, 300, 'c:\Font\BigShouldersInline-VariableFont_opsz,wght.ttf', italic=True, bold=True, rotation=180 )
+
         pygame.display.flip()
 
         # Limit the frame rate to the specified frames per second
